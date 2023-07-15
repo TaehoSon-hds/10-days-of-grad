ee62dcde0e38199ab74eb1cf37f74dad5d7436cb
Taeho Son <sth0622@haedosa.xyz>
2023-07-16 00:50:08 +0900

    Add signature to functions

--- day1/Iris.hs
+++ day1/Iris.hs
@@ -8,44 +8,53 @@
 import Numeric.LinearAlgebra as LA
 
 -- New weights
+newW :: (Int, Int) -> IO (Matrix Double)
 newW (nin, nout) = do
     let k = sqrt (1.0 / fromIntegral nin)
     w <- randn nin nout
     return (cmap (k *) w)
 
 -- Transformations
+loss :: (Container c e, Num e, Num (c e)) => c e -> c e -> e
 loss y tgt =
   let diff = y - tgt
   in sumElements $ cmap (^2) diff
 
+sigmoid :: Matrix Double -> Matrix Double
 sigmoid = cmap f
   where
     f x = recip $ 1.0 + exp (-x)
 
 -- Their gradients
+sigmoid' :: Matrix Double -> Matrix Double -> Matrix Double
 sigmoid' x dY = dY * y * (ones - y)
    where
     y = sigmoid x
     ones = (rows y) >< (cols y) $ repeat 1.0
 
+linear' :: (Fractional t, Numeric t) => Matrix t -> Matrix t -> Matrix t
 linear' x dy = cmap (/ m) (tr' x LA.<> dy)
   where
     m = fromIntegral $ rows x
 
+loss' :: (Container c b, Num b, Num (c b)) => c b -> c b -> c b
 loss' y tgt =
   let diff = y - tgt
   in cmap (* 2) diff
 
 -- Building NN
+forward :: Matrix Double -> Matrix Double -> [Matrix Double]
 forward x w1 =
   let h = x LA.<> w1
       y = sigmoid h
   in [h, y]
 
+descend :: Num a => (a -> a) -> Int -> a -> a -> [a]
 descend gradF iterN gamma x0 = take iterN (iterate step x0)
   where
     step x = x - gamma * gradF(x)
 
+grad :: (Matrix Double, Matrix Double) -> Matrix Double -> Matrix Double
 grad (x, y) w1 = dW1
   where
     [h, y_pred] = forward x w1
@@ -53,6 +62,7 @@ grad (x, y) w1 = dW1
     dY = sigmoid' h dE
     dW1 = linear' x dY
 
+main :: IO ()
 main = do
   dta <- loadMatrix "day1/iris_x.dat"
   tgt <- loadMatrix "day1/iris_y.dat"
